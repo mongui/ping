74efdb7d88a99a348d0578279e622cf26cfa0203
Gontzal Goikoetxea <mongui@users.noreply.github.com>
2018-11-14 11:24:38 +0100

    Ahora es compatible con Windows XP.
    Windows XP e inferiores no tienen el comando TIMEOUT así que ahora comprueba la versión del SO y hace un ping a localhost en estos casos.

--- ping.bat
+++ ping.bat
@@ -5,20 +5,56 @@ SET filtro=dominio1.com dominio2.com
 SET listar=NO
 SET orden=NO
 SET timeout=5
+SET timeoutdisponible=0
 
 SETLOCAL EnableDelayedExpansion
 
-ECHO Leyendo el fichero HOSTS...
+REM Sacamos la version de Windows porque el comando TIMEOUT solo esta disponible a partir de WIN7.
+ECHO Extrayendo version de Windows...
+FOR /F "skip=2 tokens=2,*" %%A IN ('reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v CurrentVersion') DO (set VERSION=%%B)
+IF "%version%" EQU "10.0" (
+	ECHO Windows 10/Windows Server 2016.
+	SET timeoutdisponible=1
+)
+IF "%version%" EQU "6.3" (
+	ECHO Windows 8.1/Windows Server 2012 R2.
+	SET timeoutdisponible=1
+)
+IF "%version%" EQU "6.2" (
+	ECHO Windows 8/Windows Server 2012.
+	SET timeoutdisponible=1
+)
+IF "%version%" EQU "6.1" (
+	ECHO Windows 7/Windows Server 2008 R2.
+	SET timeoutdisponible=1
+)
+IF "%version%" EQU "6.0" (
+	ECHO Windows Vista/Windows Server 2008.
+	SET timeoutdisponible=1
+)
+IF "%version%" EQU "5.2" (
+	ECHO Windows XP 64-Bit/Windows Server 2003.
+	SET timeoutdisponible=0
+)
+IF "%version%" EQU "5.1" (
+	ECHO Windows XP.
+	SET timeoutdisponible=0
+)
+IF "%version%" EQU "5.0" (
+	ECHO Windows 2000.
+	SET timeoutdisponible=0
+)
+ECHO.
 
 REM Lista todos los servidores del archivo de HOSTS que contengan las cadenas a filtrar.
+ECHO Leyendo el fichero HOSTS...
 SET total=0
 FOR /F "usebackq tokens=*" %%A in (%hosts%) DO (
 	SET name=%%~A
 	
 	IF /I "!name:~0,1!" NEQ "#" (
 		FOR /F "tokens=2" %%G IN ("!name!") DO (
-			Echo.%%G | findstr "%filtro%" > nul && (
-			echo %%G
+			ECHO.%%G | findstr "%filtro%" > nul && (
 				SET "servidores[!total!]=%%G"
 				SET /A total += 1
 			)
@@ -27,6 +63,7 @@ FOR /F "usebackq tokens=*" %%A in (%hosts%) DO (
 )
 
 ECHO Lectura completada.
+ECHO.
 
 REM Si no hay servidores, salimos del programa.
 IF /I "%total%" EQU "0" (
@@ -34,7 +71,6 @@ IF /I "%total%" EQU "0" (
 	ECHO No se ha encontrado ningun servidor que cumpla los parametros exigidos.
 	GOTO :salir
 )
-
 ECHO Numero de entradas seleccionadas: !total!.
 
 REM Imprime los servidores encontrados si asi se ha indicado en la variable inicial.
@@ -69,7 +105,11 @@ IF %ERRORLEVEL% GTR 0 (
 	ECHO Correcto.
 )
 
-TIMEOUT %timeout% > nul
+IF %timeoutdisponible% equ 1 (
+	TIMEOUT %timeout% > nul
+) ELSE (
+	@%SystemRoot%\system32\ping.exe -n %timeout% 127.0.0.1 > nul
+)
 GOTO :ping
 
 :salir
